9554b9123b045a74dacff28231c344aa52762551
Quentin Chateau <quentin.chateau@gmail.com>
2020-09-16 17:59:07 +0200

    fix build script

--- server/build.sh
+++ server/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export CXX=clang++-10
+export CC=clang-10
+
 mkdir -p build && cd build
 if [ ! -e CMakeCache.txt ]; then
     CXX=clang++-10 CC=clang-10 cmake .. -DCMAKE_BUILD_TYPE=Release
